d7a43bd5abb577531e6466dab6b8ad98e88a9bdf
lupor <lucassportillo09@gmail.com>
2023-07-05 19:24:13 -0300

    Desafío de programación #19: Crear una función que retorne un array con los primeros números n-primos mayores que un número particular definido alComienzo

--- algoritmos/tarea-algoritmos.js
+++ algoritmos/tarea-algoritmos.js
@@ -201,3 +201,18 @@ function imprimir100NumerosPrimos() {
 // Desafío de programación #19: Crear una función que retorne un array con los primeros números 
 // "n-primos" mayores que un número particular definido "alComienzo"
 // Ejemplo: Quiero los primeros 4 números primos mayores que 5, el resultado debería ser: [7, 11, 13, 17, 19]
+
+function imprimirNumerosPrimos(nPrimos, comienzaEn) {
+        let numerosPrimos = [];
+        let i = comienzaEn;
+
+        while (numerosPrimos.length < nPrimos) {
+            if (conseguirNumerosPrimos(i)) {
+                numerosPrimos.push(i);
+            }
+
+            i++;
+        }
+        
+        return numerosPrimos;
+}
