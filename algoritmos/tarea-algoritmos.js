34d633c50db984dd2e474c5e452c6b722eb38d3c
lupor <lucassportillo09@gmail.com>
2023-07-05 11:31:56 -0300

    Arreglos desafío de programación #18

--- algoritmos/tarea-algoritmos.js
+++ algoritmos/tarea-algoritmos.js
@@ -195,8 +195,9 @@ function calcularSumaNumeroEntero(numero) {
 
 function imprimir100NumerosPrimos() {
     const numerosPrimos = [];
-    for(let i = 0; i<100;i++){;
-        if(conseguirNumerosPrimos(i)) numerosPrimos.push(i);
-    }
-    console.log(numerosPrimos);
+    for(let i = 0; i<100;i++) if(conseguirNumerosPrimos(i)) numerosPrimos.push(i);
+    for (let x = 0; x < numerosPrimos.length; x++) console.log(numerosPrimos[x]);
 }
+// Desafío de programación #19: Crear una función que retorne un array con los primeros números 
+// "n-primos" mayores que un número particular definido "alComienzo"
+// Ejemplo: Quiero los primeros 4 números primos mayores que 5, el resultado debería ser: [7, 11, 13, 17, 19]
