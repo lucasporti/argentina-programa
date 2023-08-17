24ece7a7773ae44d04fb4c684776aa48fb03cdb6
lupor <lucassportillo09@gmail.com>
2023-08-16 23:53:37 -0300

    Desafío de programación #27: Crear una función que reciba un array de números como argumento y retorne un array con los elementos distintos

--- algoritmos/tarea-algoritmos.js
+++ algoritmos/tarea-algoritmos.js
@@ -276,3 +276,28 @@ function crearRestaEntreConjuntos(arr1,arr2){
     }
     return nuevoArr;
 }
+
+// Desafío de programación #27: Crear una función que reciba un array de números como argumento y retorne un array con los elementos distintos
+// Ejemplo:
+// [1, 2, 3, 4, 5, 4, 3, 2, 1, 0] debería retornar[1, 2, 3, 4, 5, 0]
+
+function conseguirElementosDistintos(arr) {
+    var arr1 = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!estaEnElArray(arr[i], arr1)) {
+            arr1.push(arr[i]);
+        }
+    }
+
+    return arr1;
+}
+
+function estaEnElArray(n, arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === n)
+            return true;
+    }
+
+    return false;
+}
